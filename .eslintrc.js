a72936de330981a98b6cc2842206dffb6babc5da
Vincent precious <5tcentunary@gmail.com>
2025-06-10 09:47:08 +0100

    chore: remove ESLint configuration and related dependencies from package.json and package-lock.json

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  extends: ['@react-native', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        quoteProps: 'consistent',
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'es5',
+        useTabs: false,
+      },
+    ],
+  },
+  ignorePatterns: ['node_modules/', 'lib/'],
+};
